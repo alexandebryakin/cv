3550a715a573f7f5f0fa903ff3d0eeb1ca415dea
Alexander Bryakin <alexandebryakin@gmail.com>
2024-01-11 15:34:49 +0300

    [feat] Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>TODO</div>;
+  return (
+    <BrowserRouter basename="/cv">
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<>TODO: Home Page</>} />
+        <Route path="/about" element={<>TODO: other route</>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
